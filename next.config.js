1a2a22325cabd19c4a3cd623cf851ff4e1354dfe
PDXChristian <8883699+PDXChristian@users.noreply.github.com>
2022-04-01 18:36:35 -0700

    Trying to use hashes in style-src

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const ContentSecurityPolicy =`
   form-action 'self';
   object-src 'none';
   img-src 'self' www.gstatic.com/recaptcha;
-  style-src 'self';
+  style-src 'self'
+    'sha256-GHgVb5EEAUPhVeTfwEFuX9+WADcCrePlK6CwdXznIhY='
+    'sha256-xd6kVmsB5qDY47QB4x7Ys+1t88OSTa40q/98d9NyF8Q='
+    'sha256-8BNxsIsc6VHj8/elC63fqbrGsnTOvhNTf17uhaIdUI4=',
+    'sha256-d72pVhmRTNXT2Gr2OHFRLnVaHBfiBI5EvDCF6tA924Y=';
   script-src 'self'
     https://www.recaptcha.net
     https://recaptcha.net
