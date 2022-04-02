d844e0c70853074a1c4ac3324a77c38b6dc417b1
PDXChristian <8883699+PDXChristian@users.noreply.github.com>
2022-04-01 18:04:25 -0700

    Fixed some outdated dependencies and added security headers

---
+++ next.config.js
@@ -0,0 +1,27 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self';
+  font-src 'self';
+  frame-ancestors 'none'
+`
+
+const securityHeaders = [
+  {key: 'X-XSS-Protection', value: '1; mode=block'},
+  {key: 'X-DNS-Prefetch-Control', value: 'on'},
+  {key: 'X-Frame-Options', value: 'SAMEORIGIN'},
+  {key: 'Referrer-Policy', value: 'no-referrer'},
+//  {key: 'Content-Security-Policy', value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()},
+  {key: 'X-Content-Type-Options', value: 'nosniff'}
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
