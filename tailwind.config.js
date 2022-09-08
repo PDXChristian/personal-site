c52b5b95fe180268c804c24106167ddd251b69a6
PDXChristian <8883699+PDXChristian@users.noreply.github.com>
2022-09-08 02:33:12 -0700

    Updated daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -990,4 +990,34 @@ module.exports = {
     zIndex: ['responsive', 'focus-within', 'focus'],
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    darkTheme: "mytheme",
+    lightTheme: "light",
+    themes: [
+      {
+        light: { 
+          "primary": "#570DF8",
+          "secondary": "#F000B8",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+        mytheme: { 
+          "primary": "#6419E6",
+          "secondary": "#D926A9",
+          "accent": "#1FB2A6",
+          "neutral": "#191D24",
+          "base-100": "#161925",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
 }
